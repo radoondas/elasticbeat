8cb2c0fbfb939763f87c18e9df6cd3a8d5bd679c
Rado Ondas <ondas.radovan@gmail.com>
2016-01-29 22:11:22 +0100

    Fixes #6

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	elasticbeat "github.com/radoondas/elasticbeat/beat"
 
 	"github.com/elastic/beats/libbeat/beat"
+	"os"
 )
 
 // You can overwrite these, e.g.: go build -ldflags "-X main.Version 1.0.0-beta3"
@@ -11,5 +12,8 @@ var Version = "1.0.0-dev"
 var Name = "elasticbeat"
 
 func main() {
-	beat.Run(Name, Version, elasticbeat.New())
+	err := beat.Run(Name, Version, elasticbeat.New())
+	if err != nil {
+		os.Exit(1)
+	}
 }
